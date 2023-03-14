e8dce8bd669b72eb724c8e5917af5bca71b8b132
Nick Mitchell <119639651+TheGrimmmz@users.noreply.github.com>
2023-03-14 14:15:17 -0500

    first commit

---
+++ 34_same_frequency/same_frequency.py
@@ -0,0 +1,23 @@
+def counter(col):
+    """returns counter map of col"""
+    counts = {}
+
+    for x in col:
+        counts[x] = counts.get(x, 0) + 1
+
+    return counts
+
+def same_frequency(num1, num2):
+    """Do these nums have same frequencies of digits?
+
+        >>> same_frequency(551122, 221515)
+        True
+
+        >>> same_frequency(321142, 3212215)
+        False
+
+        >>> same_frequency(1212, 2211)
+        True
+    """
+
+    return counter(str(num1)) == counter(str(num2))
